66a88b79e706ba7b74b8cde07de281028b15d258
Valp92 <117280818+Valp92@users.noreply.github.com>
2024-11-11 20:16:51 +0300

    fixed comments except 2

--- tests/swaglabs.spec.ts
+++ tests/swaglabs.spec.ts
@@ -13,15 +13,13 @@ beforeEach(async() => {
     });
     
     afterEach(async() => {
-    console.log("post-condition");
+        driver.quit();
     });
 
 it('Login', async () => {
-try {
 await driver.findElement(By.css('input[name=user-name]')).sendKeys('visual_user');  //input class
-await driver.findElement(By.css('input[id=password]')).sendKeys('secret_sauce'), 5000;  //input class
-await driver.findElement(By.xpath('//input[@class="submit-button btn_action"]')).click(), 5000; //input type
-await driver.get("https://www.saucedemo.com/inventory.html"), 6000;
+await driver.findElement(By.css('input[id=password]')).sendKeys('secret_sauce');  //input class
+await driver.findElement(By.xpath('//input[@class="submit-button btn_action"]')).click(); //input type
 await driver.findElement(By.css('div.inventory_item_label [data-test^=item-4]'));
 await driver.findElement(By.name('add-to-cart-sauce-labs-backpack')).click();
 const badgeIcon = driver.findElement(By.css('div.shopping_cart_container [data-test~="badge"]'));
@@ -30,13 +28,11 @@ await driver.findElement(By.css('div.shopping_cart_container [data-test$="link"]
 await driver.findElement(By.css('div.cart_footer [data-test="checkout"]')).click(), 5000;
 await driver.findElement(By.css('div.form_group [data-test="firstName"]')).sendKeys('TestName');
 await driver.findElement(By.css('div.form_group [data-test="lastName"]')).sendKeys('TestLastName');
-await driver.findElement(By.css('div.form_group [data-test="postalCode"]')).sendKeys('TestZipCode');
-await driver.get("https://www.saucedemo.com/checkout-step-two.html")
+await driver.findElement(By.css('div.form_group [data-test="postalCode"]')).sendKeys('TestZipCode'), 5000;
+await driver.get("https://www.saucedemo.com/checkout-step-two.html");
 await driver.findElement(By.css('div.item_pricebar [data-test*="price"]'));
 await driver.findElement(By.name('finish')).click();
 }
-finally {
-    driver.quit();
-    }}) 
+) 
 })
 
